77a77fd88de81fab4107db7370fbbf5231bc2f64
Julien Cheype <julien.cheype@vidal.fr>
2013-01-15 17:14:57 +0100

    first commit

---
+++ src/main/java/com/jcheype/codestory2013/App.java
@@ -0,0 +1,27 @@
+package com.jcheype.codestory2013;
+
+import com.jcheype.webServer.spring.SpringWebServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.lang.management.ManagementFactory;
+
+/**
+ * Hello world!
+ */
+public class App {
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        SpringWebServer springWebServer = context.getBean(SpringWebServer.class);
+        logger.debug("STARTING");
+        springWebServer.setPort(9999);
+        springWebServer.start();
+
+        String name = ManagementFactory.getRuntimeMXBean().getName();
+        logger.debug("name: ", name);
+    }
+}
